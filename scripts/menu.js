64c4b032486f57a8aafab0575204c5f4f5a4a54e
smilole <levkar787@gmail.com>
2023-04-05 21:44:55 +0700

    добавил котиков

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,31 @@
+var image = document.getElementById("kot");
+var imageName = "kotiki.jpg"; // хранит текущее имя файла изображения
+
 function goToAStar(){
     window.location.href = "A-star.html";
 }
 function goToClusterization(){
     window.location.href = "clusterization.html";
+}
+function otherButtons(){
+    window.location.href = "mainpage.html";
+}
+
+function kotus(){
+    if(imageName == "kotiki.jpg"){
+        image.src = "referenses/1.jpg";
+        imageName = "1.jpg";
+    }
+    else if(imageName == "1.jpg"){
+        image.src = "referenses/2.jpg";
+        imageName = "2.jpg";
+    }
+    else if(imageName == "2.jpg"){
+        image.src = "referenses/3.jpg";
+        imageName = "3.jpg";
+    }
+    else if(imageName == "3.jpg"){
+        image.src = "referenses/kotiki.jpg";
+        imageName = "kotiki.jpg";
+    }
 }
