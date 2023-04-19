66cf5c855b074210d4e0c1ac62b9094f03139ffd
smilole <levkar787@gmail.com>
2023-04-19 21:34:02 +0700

    лабиринт

--- scripts/A-starScript.js
+++ scripts/A-starScript.js
@@ -3,15 +3,37 @@ var table = document.getElementById("table");
 var start = null;
 var end = null;
 var impassables = new Set();
-
+var map;
 generateMap();
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function disableButtons(){
+  document.getElementById("pathButton").disabled = true;
+  document.getElementById("primmButton").disabled = true;
+  document.getElementById("genButton").disabled = true;
+}
+
+function enableButtons(){
+  document.getElementById("pathButton").disabled = false;
+  document.getElementById("primmButton").disabled = false;
+  document.getElementById("genButton").disabled = false;
+}
+
 function generateMap() {
+
+  disableButtons();
+
     n = document.getElementById("size").value;
+    map = new Array(n);
+    for(let i = 0; i < n; i++) {
+      map[i] = new Array(n);
+    for(let j = 0; j < n; j++) {
+      map[i][j] = 0;
+    }
+  }
     var newTable = document.createElement("table");
     for (var i = 0; i < n; i++) {
         var row = document.createElement("tr");
@@ -21,7 +43,7 @@ function generateMap() {
           cell.dataset.y = i;
           cell.addEventListener("click", function () {
             // При клике на ячейку устанавливаем ее как начальную или конечную
-            if (start == null && end == null &&!impassables.has(this)) {
+            if (start == null && this != end &&!impassables.has(this)) {
               start = this;
               this.classList.add("start");
             } else if (end == null && this != start && !impassables.has(this)) {
@@ -36,8 +58,8 @@ function generateMap() {
                 end = null;
                 this.classList.remove("end");
               }
-            } else if (start!=null && end!=null && this!=start && this!=end){
-                if (!impassables.has(this)) {
+            } else{
+                if (!impassables.has(this) && this!=start && this!=end) {
                     impassables.add(this);
                     this.classList.remove("searching");
                     this.classList.add("impassable");
@@ -62,9 +84,14 @@ function generateMap() {
     impassables.clear();
     pathFound = false;
     path = null;
+
+    enableButtons();
+
 }
 
 async function findPath() {
+
+  disableButtons();
 
     // Удаляем старый путь
 
@@ -80,6 +107,7 @@ async function findPath() {
 
     if (start == null || end == null) {
       alert("Укажите стартовую и конечную клетки!");
+      enableButtons();
     } else {
       var openSet = new Set([start]);
       var cameFrom = new Map();
@@ -119,6 +147,7 @@ async function findPath() {
         }
       }
       alert("Путь не найден!");
+      enableButtons();
     }
 }
 
@@ -165,6 +194,9 @@ function heuristic(a, b) {
 }
 
 async function showPath(cameFrom, current) {
+
+  let button = document.getElementById("pathButton");
+
     // Отображаем найденный путь
     while (cameFrom.has(current)) {
       current = cameFrom.get(current);
@@ -175,110 +207,209 @@ async function showPath(cameFrom, current) {
         await sleep(ms);
       }
     }
+  enableButtons();
 }
-function makeLab(){
-  // Создаем двумерный массив для представления карты, устанавливая все ячейки как стены.
-let map = [];
-for (let w = 0; w < width; w++) {
-  map[w] = [];
-  for (let h = 0; h < height; h++) {
-    map[w][h] = makeWall();
+class Coords {
+  x = null;
+  y = null;
+  constructor(x, y){
+      this.x = x;
+      this.y = y;
   }
 }
 
-// Выбираем случайную ячейку с нечетными x и y координатами и очищаем ее.
-let x = getRandomInt(0, width / 2) * 2 + 1;
-let y = getRandomInt(0, height / 2) * 2 + 1;
-map[x][y] = makeClear();
+// Сделать поле пустым
+function makeEmpty(x, y) {
+  map[x][y] = 0;
+  table.rows[x].cells[y].classList.remove("start");
+  table.rows[x].cells[y].classList.remove("end");
+  table.rows[x].cells[y].classList.remove("searching");
+  table.rows[x].cells[y].classList.remove("path");
+  table.rows[x].cells[y].classList.remove("impassable");
+  impassables.delete(table.rows[x].cells[y]);
+}
+// Сделать поле стеной
+function makeWall(x, y) {
+  map[x][y] = 1;
+  table.rows[x].cells[y].classList.remove("start");
+  table.rows[x].cells[y].classList.remove("end");
+  table.rows[x].cells[y].classList.remove("searching");
+  table.rows[x].cells[y].classList.remove("path");
+  table.rows[x].cells[y].classList.add("impassable");
+  impassables.add(table.rows[x].cells[y]);
+}
+// Проверка на tmpty поле
+function isEmpty(x, y){
+  return map[x][y] === 0 ? true : false;
+}
 
-// Создаем массив и добавляем в него валидные ячейки, которые находятся на расстоянии двух ортогональных клеток от только что очищенной клетки.
-let toCheck = [];
-if (y - 2 >= 0) {
-  toCheck.push({ x: x, y: y - 2 });
-}
-if (y + 2 < height) {
-  toCheck.push({ x: x, y: y + 2 });
-}
-if (x - 2 >= 0) {
-  toCheck.push({ x: x - 2, y: y });
-}
-if (x + 2 < width) {
-  toCheck.push({ x: x + 2, y: y });
-}
+// Создание лабиринта
+async function createPrimmLabyrinth() {
 
-// Пока есть ячейки в массиве toCheck, выбираем случайную ячейку, очищаем ее и удаляем из массива toCheck.
-while (toCheck.length > 0) {
-  let index = getRandomInt(0, toCheck.length);
-  let cell = toCheck[index];
-  x = cell.x;
-  y = cell.y;
-  map[x][y] = makeClear();
-  toCheck.splice(index, 1);
+  disableButtons();
 
-  // Очищенная ячейка должна быть соединена с другой свободной ячейкой.
-  // Ищем свободную ячейку на расстоянии двух ортогональных клеток от только что очищенной ячейки.
-  let directions = ['NORTH', 'SOUTH', 'EAST', 'WEST'];
-  while (directions.length > 0) {
-    let dirIndex = getRandomInt(0, directions.length);
-    let direction = directions[dirIndex];
-    switch (direction) {
-      case 'NORTH':
-        if (y - 2 >= 0 && map[x][y - 2] === makeClear()) {
-          map[x][y - 1] = makeClear();
-          directions = [];
-        }
-        break;
-      case 'SOUTH':
-        if (y + 2 < height && map[x][y + 2] === makeClear()) {
-          map[x][y + 1] = makeClear();
-          directions = [];
-        }
-        break;
-      case 'EAST':
-        if (x - 2 >= 0 && map[x - 2][y] === makeClear()) {
-          map[x - 1][y] = makeClear();
-          directions = [];
-        }
-        break;
-      case 'WEST':
-        if (x + 2 < width && map[x + 2][y] === makeClear()) {
-          map[x + 1][y] = makeClear();
-          directions = [];
-        }
-        break;
+  let size = document.getElementById("size").value;
+
+  // Создание массива лабиринта
+  let map = new Array(size);
+  for(let i = 0; i < size; i++) {
+      map[i] = new Array(size);
+  }
+
+
+
+  let topRow = 0;
+  let bottomRow = size - 1;
+  let topCell = 0;
+  let bottomCell = size - 1;
+
+  while(topRow<=bottomRow){
+    while(topCell<size && bottomCell>=0){
+      makeWall(topRow,topCell);
+      makeWall(bottomRow,bottomCell);
+
+      topCell++;
+      bottomCell--;
     }
-    directions.splice(dirIndex, 1);
+    topCell = 0;
+    bottomCell = size - 1;
+    topRow++;
+    bottomRow--;
   }
 
-  // Добавляем в массив toCheck все валидные ячейки, находящиеся на расстоянии двух ортогональных клеток от только что очищенной ячейки и которые еще не добавлены в массив toCheck.
-  if (y - 2 >= 0 && map[x][y - 2] === makeWall()) {
-    toCheck.push({ x: x, y: y - 2 });
+
+  // Рандомная ячейка начала генерации лабиринта
+  let cell = new Coords(Math.floor((Math.random() * (size / 2))) * 2, Math.floor((Math.random() * (size / 2))) * 2); 
+  makeEmpty(cell.x, cell.y);
+
+  // массив использованных ячеек
+  let isUsed = new Array(size);
+  for(let i = 0; i < size; i++){
+      isUsed[i] = new Array(size);
+      for(let j = 0; j < size; j++) {
+          isUsed[i][j] = false;
+      }
   }
-  if (y + 2 < height && map[x][y + 2] === makeWall()) {
-    toCheck.push({ x: x, y: y + 2 });
+  isUsed[cell.x][cell.y] = true;
+
+  // Создание массива и добавление туда точек лабиринта находящиеся в двух клетках от координаты которую выбрали выше.
+  let toCheck = new Array;
+  if (cell.y - 2 >= 0) {
+      toCheck.push(new Coords(cell.x, cell.y - 2));
+      isUsed[cell.x][cell.y - 2] = true;
   }
-  if (x - 2 >= 0 && map[x - 2][y] === makeWall()) {
-    toCheck.push({ x: x - 2, y: y });
+  if (cell.y + 2 < size) {
+      toCheck.push(new Coords(cell.x, cell.y + 2));
+      isUsed[cell.x][cell.y + 2] = true;
   }
-  if (x + 2 < width && map[x + 2][y] === makeWall()) {
-    toCheck.push({ x: x + 2, y: y });
+  if (cell.x - 2 >= 0) {
+      toCheck.push(new Coords(cell.x - 2, cell.y));
+      isUsed[cell.x - 2][cell.y] = true;
+  }
+  if (cell.x + 2 < size) {
+      toCheck.push(new Coords(cell.x + 2, cell.y));
+      isUsed[cell.x + 2][cell.y] = true;
+  }
+
+  // Счетчик для скипа задержек в анимации
+  let count = 0;
+
+  // Пока есть элементы в массиве, выбрать рандомный и убрать стены.
+  while (toCheck.length > 0) {
+      let index = Math.floor(Math.random() * toCheck.length);
+      let x = toCheck[index].x;
+      let y = toCheck[index].y;
+      makeEmpty(x, y);
+
+      if(x == size - 2) {
+        makeEmpty(x + 1, y);
+      }
+      if(y == size - 2) {
+        makeEmpty(x, y + 1);
+      }
+      toCheck.splice(index, 1);
+
+
+      // Убарть стену в ячейке находящейся между рандомной ячейкой и ее родителем.
+      let directions = ["NORTH", "SOUTH", "EAST", "WEST"];
+      let flag = false;
+      while (directions.length > 0 && !flag) {
+          let dir_index = Math.floor(Math.random() * directions.length);
+          switch (directions[dir_index]) {
+          case "NORTH":
+              if ( y - 2 >= 0 && isEmpty(x, y - 2)) {
+                  makeEmpty(x, y - 1);
+                  flag = true;
+              }
+              break;
+          case "SOUTH":
+              if (y + 2 < size && isEmpty(x, y + 2)) {
+                  makeEmpty(x, y + 1);
+                  flag = true;
+              }
+              break;
+          case "EAST":
+              if (x - 2 >= 0 && isEmpty(x - 2, y)) {
+                  makeEmpty(x - 1, y);
+                  flag = true;
+              }
+              break;
+          case "WEST":
+              if (x + 2 < size && isEmpty(x + 2, y)) {
+                  makeEmpty(x + 1, y);
+                  flag = true;
+              }
+              break;
+          }
+          directions.splice(dir_index, 1);
+      }
+
+
+
+
+      // Добавить новые клетки которые можно зачистить.
+      if (y - 2 >= 0 && !isEmpty(x, y - 2) && !isUsed[x][y - 2]) {
+          toCheck.push(new Coords(x, y - 2));
+          isUsed[x][y - 2] = true;
+      }
+      if (y + 2 < size && !isEmpty(x, y + 2) && !isUsed[x][y + 2]) {
+          toCheck.push(new Coords(x, y + 2));
+          isUsed[x][y + 2] = true;
+      }
+      if (x - 2 >= 0 && !isEmpty(x - 2, y) && !isUsed[x - 2][y]) {
+          toCheck.push(new Coords(x - 2, y));
+          isUsed[x - 2][y] = true;
+      }
+      if (x + 2 < size && !isEmpty(x + 2, y) && !isUsed[x + 2][y]) {
+          toCheck.push(new Coords(x + 2, y));
+          isUsed[x + 2][y] = true;
+      }
+
+      if(count >= Math.floor(size / 10)){
+          await sleep(document.getElementById("animation").value);
+          count = 0;
+      }
+      count++;
+  }
+  if(size % 2 == 0){
+  makeEmpty(size - 1, size - 1)
+  if(Math.floor((Math.random() * (2))) == 1) {
+    makeEmpty(size - 2, size - 1)
+    makeWall(size - 1, size - 2)
+  } else {
+    makeEmpty(size - 1, size - 2)
+    makeWall(size - 2, size - 1)
   }
 }
 
-// Функция для создания стены
-function makeWall() {
-  return 1; // предполагаем, что 1 - это представление стены на карте
-}
+ 
 
-// Функция для создания прохода
-function makeClear() {
-  return 0; // предполагаем, что 0 - это представление прохода на карте
-}
+  // Поставить дефолтное значение для старта и финиша.
+  table.rows[0].cells[0].classList.add("start");
+  start = table.rows[0].cells[0];
+  table.rows[size - 1].cells[size - 1].classList.add("end");
+  end = table.rows[size - 1].cells[size - 1];
 
-// Функция для генерации случайного целого числа в заданном диапазоне
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
+  enableButtons();
 
 }
